1c33f171525d9ee2daf08ef79f76af32ab015334
Zach Pfeifer <mr.zman1@yahoo.com>
2019-08-14 16:29:45 -0600

    Bad Finish Game

--- app.js
+++ app.js
@@ -8,4 +8,44 @@ const scoreboard = {
   player: 0,
   computer: 0
 }
+
+
 //Game JS
+//Game Logic
+function play(playerChoice) {
+  //Choices
+  let choices = ["Rock", "Paper", "Scissors"];
+
+  //Computer Logic
+  let compchoice = choices[Math.floor(Math.random() * choices.length)];
+  console.log(compchoice)
+
+  //NOTE CHOOSE PAPER:
+  if (playerChoice == "Paper") {
+    if (playerChoice == compchoice) {
+      alert("Draw")
+    } else if (compchoice == "Rock") {
+      alert("Paper beats Rock \n You Win\n")
+    } else {
+      alert("Paper doesn't beat Scissors \nYou Lose\n")
+    };
+  }//NOTE CHOOSE ROCK
+  else if (playerChoice == "Rock") {
+    if (playerChoice == compchoice) {
+      alert("Draw")
+    } else if (compchoice == "Paper") {
+      alert("Rock doesn't beat Paper \nYou Lose\n")
+    } else {
+      alert("Rock beats Scissors \nYou Win\n")
+    };
+  }//NOTE CHOOSE SCISSORS
+  else {
+    if (playerChoice == compchoice) {
+      alert("Draw")
+    } else if (compchoice == "Rock") {
+      alert("Scissors doesn't beat Rock \nYou Lose\n")
+    } else {
+      alert("Scissors beats Paper \nYou Win\n")
+    };
+  }
+}
